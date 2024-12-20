26774f9b13a973411f903ac8220268c366cec569
Tanishka <tanishkadeep09@gmail.com>
2024-12-20 06:40:50 +0530

    bug

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,16 @@ import { ModeToggle } from "@/components/ThemeToggle";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Logo from "@/app/icon.png";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="sticky top-0 backdrop-blur-xl">
       <div className="flex justify-between items-center px-8 sm:px-12 md:px-24 py-4 border-b-2 shadow-md max-w-screen-2xl mx-auto">
-        <div className="flex items-center gap-2">
+        <Link href={"/"} className="flex items-center gap-2">
           <Image src={Logo} alt="logo" width={35} height={35} />
           <div className="md:text-2xl text-lg font-extrabold">ContiqAI</div>
-        </div>
+        </Link>
         <div className="flex items-center gap-4 justify-center font-bold">
           <ModeToggle />
           <SignedOut>
